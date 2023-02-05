3d2ca9bd90e6339048f6c9a96ed02cb2d4ea6031
Gadjimurad Asirdionov <gadjimurad.asirdionov@gridnine.com>
2023-02-05 15:57:39 +0300

    code splitting for routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import { Counter } from './components/Counter';
 import './index.scss'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 export default function App() {
   return (
     <div className='app'>
-      ewjghfaoliewef
-      <Counter />
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync />}/>
+          <Route path={'/'} element={<MainPageAsync/>}/>
+        </Routes>
+      </Suspense>
     </div>
   )
 }
