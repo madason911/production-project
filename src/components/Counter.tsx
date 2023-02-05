de5f7449eca2d8158aeeba919bd3a8abb7cbe48f
Gadjimurad Asirdionov <gadjimurad.asirdionov@gridnine.com>
2023-02-05 14:26:21 +0300

    react connection and css setup

---
+++ src/components/Counter.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import './Counter.scss'
+
+export default function Counter() {
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => { 
+    setCount(count + 1)
+   }
+
+  return (
+    <div>
+      <div>{count}</div>
+      <button onClick={increment}>increment</button>
+    </div>
+  )
+}
