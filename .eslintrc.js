601e60a8e01e754f90b4f7f48b9f39d1e3239661
madason911 <mr.asirdinov@mail.ru>
2024-01-30 23:31:07 +0300

    add loader and not found page

--- .eslintrc.js
+++ .eslintrc.js
@@ -48,6 +48,7 @@ module.exports = {
         'react/function-component-definition': 'off',
         'no-underscore-dangle': 'off',
         'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'max-len': ['error', { ignoreComment: true }],
     },
     globals: {
         __IS_DEV__: true,
